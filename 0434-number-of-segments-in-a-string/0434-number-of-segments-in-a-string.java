c608d34b111e24c0a4acaf4df1351dda9b440fde
Harsh Sahu <119934930+Harshsahu001@users.noreply.github.com>
2024-09-29 19:44:31 +0530

    Time: 0 ms (100%), Space: 41.4 MB (28.14%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -1,7 +1,14 @@
 import java.util.*;
 class Solution {
     public int countSegments(String s) {
-        StringTokenizer st = new StringTokenizer(s);
-        return st.countTokens();
+        // StringTokenizer st = new StringTokenizer(s);
+        // return st.countTokens();
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' '))
+            ans++;
+        }
+        return ans;
     }
 }
