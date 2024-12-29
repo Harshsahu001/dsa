d42c4d5d433b11676a02c2272caebd01eafbdf14
Harsh Sahu <119934930+Harshsahu001@users.noreply.github.com>
2024-12-29 23:25:17 +0530

    Time: 6 ms (24.62%), Space: 42.3 MB (8.71%) - LeetHub

---
+++ 1449-print-words-vertically/1449-print-words-vertically.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<String> printVertically(String s) {
+       String[] array = s.split(" ");
+        int i=0;
+        String res=" ";
+        int mal=0;
+        int mil=999;
+        for(String str : array)
+        {
+            mal =Math.max(str.length(),mal);
+            mil =Math.min(str.length(),mil);
+        }
+        ArrayList<String> result= new ArrayList<>();
+        for(i=0;i<mal;i++)
+        {
+            res="";
+            for(int j=0;j<array.length;j++)
+            {
+                if(i<array[j].length())
+                {
+               
+                String word=array[j];
+                res+=word.charAt(i);
+                }
+                else{
+                  res+=" ";
+                }
+            }
+            for(int k =res.length()-1;k>=0;k--)
+            {
+                if(res.charAt(k)!=' ')
+                {
+                    res=res.substring(0,k+1);
+                    break;
+                }
+            }
+            result.add(res);
+        }
+    return result;
+    }
+}
