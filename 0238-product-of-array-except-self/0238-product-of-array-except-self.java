986438ef47bef0b892b66db08fb9afab18092543
Harsh Sahu <119934930+Harshsahu001@users.noreply.github.com>
2025-01-02 22:49:43 +0530

    Time: 2 ms (89.31%), Space: 55.5 MB (46.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,19 +4,18 @@ class Solution {
         int [] pre= new int[n];
         int [] suff= new int[n];
         int [] arr= new int[n];
-        pre[0]=1;
+        int p=1;
+        arr[0]=1;
         for(int i=1;i<n;i++)
         {
-            pre[i]=pre[i-1]*nums[i-1];
+            p=p*nums[i-1];
+            arr[i]=p;
         }
-        suff[n-1]=1;
+        p=1;
         for(int i=n-2;i>=0;i--)
         {
-            suff[i]=suff[i+1]*nums[i+1];
-        }
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=suff[i]*pre[i];
+            p=p*nums[i+1];
+            arr[i]=arr[i]*p;
         }
         return arr;
     }
